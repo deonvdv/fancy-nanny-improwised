1e6c6ee4641612a87e4e0e8b3e0b790ff98cf373
Shailesh Davara <davara.shailesh@gmail.com>
2014-01-22 19:44:07 +0530

    Rename Ingrendient to ingredient.

---
+++ app/models/Model/RecipeIngredient.php
@@ -0,0 +1,9 @@
+<?php
+namespace Model;
+class RecipeIngredient extends BaseModel {
+	protected $guarded = array('id');
+	public static $rules = array();
+	public function getRecipeIngredientsByRecipe($recipe_id) {
+		return $this->where('recipe_id', '=', $recipe_id)->get();
+	}
+}
--- app/models/Model/RecipeIngrendient.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Model;
-class RecipeIngrendient extends BaseModel {
-	protected $guarded = array('id');
-
-	public static $rules = array();
-}

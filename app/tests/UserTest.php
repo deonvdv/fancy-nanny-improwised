a75d989ebd7726e323dfbad8568d96d943fac326
Shailesh Davara <davara.shailesh@gmail.com>
2014-01-31 19:32:12 +0530

    --Added new scenarios in user test case.

--- app/tests/UserTest.php
+++ app/tests/UserTest.php
@@ -16,47 +16,87 @@ class UserTest extends TestCase {
 		$household = \Models\Household::where('name','like','%household')->first();
 		$roles = ['parent','guardian','child','staff'];
 		
-		$newuser = new \Models\User();
-		$newuser->name = $faker->name;
-        $newuser->email = $faker->email;
-        $newuser->password = Hash::make($faker->word . strtoupper($faker->randomLetter) . $faker->randomDigitNotNull . $faker->word);
-        $newuser->street = $faker->streetAddress;
-        $newuser->city = $faker->city;
-        $newuser->state = $faker->state;
-        $newuser->zip = $faker->postcode;
-        $newuser->country = substr($faker->country,0,50);
-        $newuser->home_number = $faker->optional($weight = 0.5)->phoneNumber;
-        $newuser->work_number = $faker->optional($weight = 0.5)->phoneNumber;
-        $newuser->role = $roles[rand(0, 3)];                
-        $newuser->app_settings = json_encode( array("preferred_notification" => rand(0, 1) ? 'email' : 'text' ) );
+		 $tmp = [
+            'name'               => "Shailesh". $faker->word,
+            'household_id'       => $household->id,
+            'email'              => $faker->email,
+            'password'           => Hash::make("xxx"),
+            'street'             => "My Street",
+            'city'               => "Cadiz",
+            'state'              => "Cadiz",
+            'zip'                => "11011",
+            'country'            => "Spain",
+            'home_number'        => "",
+            'work_number'        => "",
+            'role'               => "admin",
+            'app_settings'       => json_encode( array("preferred_notification" => 'email' ) ),
+        ];
 
+        $newuser = new \Models\User( $tmp );
         $newuser->save();
-		
+       
 		//associate household
 		$newuser->household()->associate($household);
         
 		//Set Profile picture
 		$pic = new \Models\Picture( 
-                array('name' => 'profile_pic_' . $faker->word, 
+                array('name' => 'profile_pic_' . $faker->word, 'imageable_id'=>$newuser->id,
+                	'imageable_type' =>'User',
                       'cdn_url' => $faker->word, 'file_name' => $faker->word.".".$faker->fileExtension) );
         $newuser->profile_picture()->save($pic);
+
+        //Add Documents
+        $doc = new \Models\Document( array('name' => 'doc_name' . $faker->word, 
+        				'private' => $faker->boolean,
+        			    'cdn_url' => $faker->word, 
+        			    'household_id' => $household->id,
+        			    'file_name' => $faker->word.".".$faker->fileExtension) );
+        $newuser->documents()->save($doc);
+
+        //Add Messages
+        $msg = new \Models\Message( array('message' => 'message_test' . $faker->word, 
+        				'date' => $faker->date,
+        			    'household_id' => $household->id) );
+        $newuser->messages()->save($msg);
+
+         //Add Notifications
+        $notification = new \Models\Notification( array('message' => 'message_test' . $faker->word, 
+        				'to' => substr('from msg:' . $faker->word,0,36),
+        				'reference' => substr('ref:' . $faker->word,0,50),
+        			    'household_id' => $household->id) );
+        $newuser->notifications()->save($notification);
 
         $id = $newuser->id;
         
 		// Get User from database
-		$found = \Models\User::with( array('household') )->where('id', '=', $id)->firstOrFail();
-		
+		$found = \Models\User::with( array('household','profile_picture',
+			'documents','messages','notifications') )->where('id', '=', $id)->firstOrFail();
 		$this->assertTrue($found->id == $id);
 
 		// Test Household
 		$this->assertTrue($found->household->id == $household->id);
 		$this->assertTrue($found->household->name == $household->name);
 
+		//Test profile_picture
+		$this->assertTrue($found->profile_picture->id == $pic->id);
+		$this->assertTrue($found->profile_picture->name == $pic->name);
+		$this->assertTrue($found->profile_picture->cdn_url == $pic->cdn_url);
+
+		//Test documents
+		$this->assertTrue(count($found->documents) == 1);
+
+		//Test messages
+		$this->assertTrue(count($found->messages) == 1);
+
+		//Test notifications
+		$this->assertTrue(count($found->notifications) == 1);
+
 		// Test User
 		$this->assertTrue($found->id == $newuser->id);
 		$this->assertTrue($found->name == $newuser->name);
 		$this->assertTrue($found->street == $newuser->street);
-		$this->assertTrue($found->city == $unitofmeasure->city);
+		$this->assertTrue($found->city == $newuser->city);
+		
 	}
 
 }

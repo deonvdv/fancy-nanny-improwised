cd51deef9211b3b13f411aa3e61d6f986bab64fc
tapanbavaliya <tbavaliya@gmail.com>
2014-05-30 19:50:48 +0530

    data bind in eventdetail.html

--- public/app/js/controllers/EventsController.js
+++ public/app/js/controllers/EventsController.js
@@ -1,8 +1,26 @@
 angular.module('myApp')
 
     // event controller ------------------------------------------------------------------------------
-    .controller('eventsController',function($scope, $controller ,$http ){
+    .controller('eventsController',function($scope, $controller ,$http ,Events){
 
         $controller('homeController', {$scope: $scope});
+
+        // ==============================================================================
+
+        // object to hold all the data for events
+
+        $scope.events = {};
+
+        loadEvents();
+
+        function loadEvents(){
+            //Fetch all Fevorite Recipes for LoggedIn User.
+            Events.get()
+                .success(function(data) {
+                    $scope.events = data.data;
+            });
+        }
+
+        // ==============================================================================
 
     });

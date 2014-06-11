53d2b22a0b5a099808379bbf4d29c920c8d03e84
tapanbavaliya <davara.shailesh@gmail.com>
2014-06-11 18:02:51 +0530

    added functionality of add tags in recipe retail.

--- public/app/js/controllers/recipeDetailController.js
+++ public/app/js/controllers/recipeDetailController.js
@@ -1,7 +1,7 @@
 angular.module('myApp')
 
     // recipes controller ------------------------------------------------------------------------------
-    .controller('recipeDetailController',function($scope, $http, $controller, $routeParams, Recipes){
+    .controller('recipeDetailController',function($scope, $http, $controller, $routeParams, Recipes, Users,$timeout){
 
         $controller('homeController', {$scope: $scope});
 
@@ -18,6 +18,64 @@ angular.module('myApp')
                 .success(function(response){
                     $scope.recipeDetail = response.data;
             });
+
+        };
+
+        // ==============================================================================
+
+        $scope.tags = {};
+
+        loadData();
+
+        function loadData(){
+            //Fetch all tags for LoggedInUser
+            Users.getTags(sessionStorage.loggedUserId)
+                .success(function(data){
+                    $scope.tags = data.data;
+            });
+        }
+
+        // ==============================================================================
+
+        $scope.sucess = false;
+
+        $scope.done = function(){
+            $timeout(function () { $scope.sucess = false; }, 3000);
+        };
+
+        // ==============================================================================
+
+        $scope.dropCallback = function (event, ui, recipeDetail, tag) {
+
+            var last_tag = recipeDetail.tags.length;
+
+            if(last_tag === 1){
+
+                console.log(recipeDetail.tags[last_tag-1]);
+
+            }
+
+            else{
+
+                for(i=0;i<last_tag-1;i++){
+                    if ( recipeDetail.tags[i].id === recipeDetail.tags[last_tag-1].id ){
+                        console.log("tag is already in list");
+                        recipeDetail.tags.pop(recipeDetail.tags[last_tag-1]);
+                        console.log(recipeDetail.tags);
+                        $scope.sucess = true;
+                        $scope.done();
+                        break;
+                    }
+                }
+
+                if(recipeDetail.tags[last_tag-1] === undefined){
+                    console.log("not add");
+                }
+                else{
+                    console.log("add");
+                }
+
+            }
 
         };
 

9da1a1fad35f3e00ba1b50c886a186d2f1b4b4dd
tapanbavaliya <davara.shailesh@gmail.com>
2014-06-11 18:06:35 +0530

    added functionality of add tags in event detail.

--- public/app/js/controllers/eventDetailController.js
+++ public/app/js/controllers/eventDetailController.js
@@ -1,7 +1,7 @@
 angular.module('myApp')
 
     // event Detail controller ------------------------------------------------------------------------------
-    .controller('eventDetailController',function($scope, $controller ,$http ,Events , $routeParams ){
+    .controller('eventDetailController',function($scope, $controller, $http, Events, $routeParams, Users, $timeout){
 
         $controller('homeController', {$scope: $scope});
 
@@ -21,5 +21,65 @@ angular.module('myApp')
             });
 
         };
+
+        // ==============================================================================
+
+        $scope.tags = {};
+
+        loadData();
+
+        function loadData(){
+            //Fetch all tags for LoggedInUser
+            Users.getTags(sessionStorage.loggedUserId)
+                .success(function(data){
+                    $scope.tags = data.data;
+            });
+        }
+
+        // ==============================================================================
+
+        $scope.sucess = false;
+
+        $scope.done = function(){
+            $timeout(function () { $scope.sucess = false; }, 3000);
+        };
+
+        // ==============================================================================
+
+        $scope.dropCallback = function (event, ui, eventDetail, tag) {
+
+            var last_tag = eventDetail.tags.length;
+
+            if(last_tag === 1){
+
+                console.log(eventDetail.tags[last_tag-1]);
+
+            }
+
+            else{
+
+                for(i=0;i<last_tag-1;i++){
+                    if ( eventDetail.tags[i].id === eventDetail.tags[last_tag-1].id ){
+                        console.log("tag is already in list");
+                        eventDetail.tags.pop(eventDetail.tags[last_tag-1]);
+                        console.log(eventDetail.tags);
+                        $scope.sucess = true;
+                        $scope.done();
+                        break;
+                    }
+                }
+
+                if(eventDetail.tags[last_tag-1] === undefined){
+                    console.log("not add");
+                }
+                else{
+                    console.log("add");
+                }
+
+            }
+
+        };
+
+        // ==============================================================================
 
     });

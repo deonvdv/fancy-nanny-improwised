85d79d5556551282b50d9b5ee3cc7212bf37d6bf
tapanbavaliya <davara.shailesh@gmail.com>
2014-06-19 11:47:45 +0530

    categories the meal according to their week no in Meals.js

--- public/app/js/services/Meals.js
+++ public/app/js/services/Meals.js
@@ -3,7 +3,74 @@ angular.module('myApp')
     .factory('Meals', function($http) {
         return {
             get : function() {
-                return $http.get('/api/v1/meal');
+
+                var meals = [];
+
+                var day_of_week = [];
+
+                var day_of_week_1 = [];
+                day_of_week_1.day = "Sunday";
+
+                var day_of_week_2 = [];
+                day_of_week_2.day = "Monday";
+
+                var day_of_week_3 = [];
+                day_of_week_3.day = "Tuesday";
+
+                var day_of_week_4 = [];
+                day_of_week_4.day = "Wednesday";
+
+                var day_of_week_5 = [];
+                day_of_week_5.day = "Thursday";
+
+                var day_of_week_6 = [];
+                day_of_week_6.day = "Friday";
+
+                var day_of_week_7 = [];
+                day_of_week_7.day = "Saturday";
+
+                $http.get('/api/v1/meal').success(function(data){
+
+                    meals = data.data;
+
+                    for(i=0;i<meals.length;i++){
+
+                        if(meals[i].day_of_week === 1){
+                            day_of_week_1.push(meals[i]);
+                        }
+                        else if(meals[i].day_of_week === 2){
+                            day_of_week_2.push(meals[i]);
+                        }
+                        else if(meals[i].day_of_week === 3){
+                            day_of_week_3.push(meals[i]);
+                        }
+                        else if(meals[i].day_of_week === 4){
+                            day_of_week_4.push(meals[i]);
+                        }
+                        else if(meals[i].day_of_week === 5){
+                            day_of_week_5.push(meals[i]);
+                        }
+                        else if(meals[i].day_of_week === 6){
+                            day_of_week_6.push(meals[i]);
+                        }
+                        else if(meals[i].day_of_week === 7){
+                            day_of_week_7.push(meals[i]);
+                        }
+
+                    }
+
+                    day_of_week.push(day_of_week_1);
+                    day_of_week.push(day_of_week_2);
+                    day_of_week.push(day_of_week_3);
+                    day_of_week.push(day_of_week_4);
+                    day_of_week.push(day_of_week_5);
+                    day_of_week.push(day_of_week_6);
+                    day_of_week.push(day_of_week_7);
+
+                });
+
+                return day_of_week;
+
             },
             show : function(id) {
                 return $http.get('/api/v1/meal/' + id);
